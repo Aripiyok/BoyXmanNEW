003fc661e3244ef3f3ee50ad56c2bd1204500dfe
Ariav <86340085+Aripiyok@users.noreply.github.com>
2022-01-21 03:37:13 +0700

    Add files via upload

---
+++ Yukki/Plugins/Playlist.py
@@ -0,0 +1,110 @@
+from pyrogram import filters
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                            KeyboardButton, Message, ReplyKeyboardMarkup,
+                            ReplyKeyboardRemove)
+
+from Yukki import BOT_ID, BOT_USERNAME, MUSIC_BOT_NAME, SUDOERS, app, db_mem
+from Yukki.Database import (_get_playlists, delete_playlist, get_playlist,
+                            get_playlist_names, save_playlist)
+from Yukki.Decorators.admins import AdminRightsCheck
+from Yukki.Decorators.assistant import AssistantAdd
+from Yukki.Decorators.checker import checker, checkerCB
+from Yukki.Decorators.permission import PermissionCheck
+from Yukki.Inline import (add_genre_markup, check_genre_markup, check_markup,
+                          delete_playlist_markuup, download_markup,
+                          others_markup, play_genre_playlist, playlist_markup,
+                          third_playlist_markup)
+
+
+@app.on_callback_query(filters.regex(pattern=r"show_genre"))
+async def show_genre(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    a, b, c = callback_request.split("|")
+    buttons = play_genre_playlist(a, b, "abcd")
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+
+@app.on_callback_query(filters.regex(pattern=r"playlist_check"))
+async def playlist_check(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    a, b, c = callback_request.split("|")
+    print(b)
+    buttons = check_genre_markup(b, "abcd", userid)
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+
+@app.on_callback_query(filters.regex(pattern=r"main_playlist"))
+async def main_playlist(_, CallbackQuery):
+    await CallbackQuery.answer()
+    user_id = CallbackQuery.from_user.id
+    user_name = CallbackQuery.from_user.first_name
+    buttons = playlist_markup(user_name, user_id, "abcd")
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+
+@app.on_callback_query(filters.regex(pattern=r"your_playlist"))
+async def your_playlist(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    buttons = add_genre_markup(user_id, "Personal", videoid)
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+
+@app.on_callback_query(filters.regex(pattern=r"group_playlist"))
+async def group_playlist(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    buttons = add_genre_markup(user_id, "Group", videoid)
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+
+@app.on_callback_query(filters.regex(pattern=r"other"))
+async def otherhuvai(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    buttons = others_markup(videoid, user_id)
+    db_mem[videoid]["check"] = 1
+    await CallbackQuery.edit_message_reply_markup(
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+
+@app.on_callback_query(filters.regex(pattern=r"goback"))
+async def goback(_, CallbackQuery):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    userid = CallbackQuery.from_user.id
+    videoid, user_id = callback_request.split("|")
+    buttons = others_markup(videoid, user_id)
+    try:
+        await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except:
+        pass
